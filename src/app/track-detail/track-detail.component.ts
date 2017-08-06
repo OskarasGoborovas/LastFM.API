6ba31745423d024bddeef8d991ed8f304f11b72f
OskarasG <oskagobo@gmail.com>
2017-08-07 01:11:25 +0200

    onClick works! Next step retrieving artist's info

--- src/app/track-detail/track-detail.component.ts
+++ src/app/track-detail/track-detail.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {Track} from '../track';
 
 @Component({
   selector: 'app-track-detail',
-  // templateUrl: './track-detail.component.html',
-  // styleUrls: ['./track-detail.component.css']
-  template: `
+    template: `
     <div *ngIf="track">
       <h2>{{track.name}} details!</h2>
-      <div><label>id: </label>{{track.id}}</div>
     </div>
   `
 })
-export class TrackDetailComponent implements OnInit {
+export class TrackDetailComponent {
 
-  constructor() { }
+  @Input() track: Track;
 
-  ngOnInit() {
-  }
 
 }
